f9537fa812b065a2dd85517c0482f495c4a67f6a
J <88823380+jijirepo@users.noreply.github.com>
2023-12-29 22:10:19 -0500

    Settings menu, only run on reviews

--- userscript.js
+++ userscript.js
@@ -4,17 +4,27 @@
 // @version      0.1
 // @description  Rings a bell when you've taken too long to answer
 // @author       Jullace
-// @match        https://www.wanikani.com/*
-// @match        https://preview.wanikani.com/*
+// @match        https://www.wanikani.com/dashboard
+// @match        https://www.wanikani.com/subjects/*
+// @match        https://preview.wanikani.com/subjects/*
+
 // @grant        none
 // ==/UserScript==
 
 ;(async function ($, wkof) {
+    let settings = {}
+    let timeUntil
+    var href = window.location.pathname
+
+
+    // Script info
+    const script_id = 'wakeup_bell'
+    const script_name = 'Wake-up Bell'
 
     // Initiate WKOF
     await confirm_wkof()
-    wkof.include('Menu,Settings')
-    wkof.ready('Settings,Menu,Apiv2')
+    wkof.include('Settings,Menu')
+    wkof.ready('Settings,Menu').then(load_settings).then(install_menu)
 
     // Makes sure that WKOF is installed
     async function confirm_wkof() {
@@ -29,23 +39,64 @@
         }
     }
 
-    console.log("I am loaded")
-
-    var countdown = 10;
-    var setTime = 10;
-
-    var timer = setInterval(function(){
-        if (countdown <= 0) {
-            console.log("times up.");
+    // Load WKOF settings
+    async function load_settings() {
+        const defaults = {
+            time: 15
         }
+        settings = await wkof.Settings.load(script_id, defaults)
+        return settings
+    }
 
-        countdown -= 1;
-    }, setTime * 100);
+    // Installs the options button in the menu
+    function install_menu() {
+        const config = {
+            name: script_id,
+            submenu: 'Settings',
+            title: script_name,
+            on_click: open_settings,
+        }
+        wkof.Menu.insert_script_link(config)
+    }
 
-    // Detect change in SRS
-    window.addEventListener('didAnswerQuestion', (e) => {
-        countdown = 10;
-    })
+    // Opens settings dialogue when button is pressed
+    function open_settings() {
+        const config = {
+            script_id,
+            title: script_name,
+            on_save: () => {},
+            content: {
+                bell: {
+                    type: 'number',
+                    default: 15,
+                    label: 'Timer duration',
+                    hover_tip: 'Duration until warning timer will ring (5 seconds minimum)',
+                    min: 5
+                },
+            },
+        }
+        new wkof.Settings(config).open()
+    }
+
+    console.log('i am loaded')
+
+    // Only run script on review or extra study pages
+    if (href.includes('review') || href.includes('extra_study')) {
+        console.log('script running')
+
+        var countdown = 15
+        setInterval(() => {
+            if (countdown == 0) {
+                console.log("times up.")
+            }
+            countdown -= 1
+        }, 15 * 100)
+
+        // Detect change in SRS
+        window.addEventListener('didAnswerQuestion', (e) => {
+            countdown = 10
+        })
+    }
 
 
 })(window.jQuery, window.wkof)
